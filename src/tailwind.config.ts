6c1b75c80a1fadcdcd6df6087f74bdaaaf2af902
Bruno Oliveira <ylhito0307@gmail.com>
2025-08-06 20:55:02 +0000

    nao gostei, usa um Sans serif mesmo

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
