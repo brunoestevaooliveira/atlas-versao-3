b4b8c06d301b0e0071f05740ba4db41ab5eed3dd
Bruno Oliveira <ylhito0307@gmail.com>
2025-09-03 19:43:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -35,10 +35,9 @@ const ThemeToggle = () => {
       variant="ghost"
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="!bg-muted !border-border/50 border rounded-full transition-all duration-300 hover:!bg-muted/80 hover:scale-105 hover:shadow-md"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-white" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
