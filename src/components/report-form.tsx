242bb112a44c553fde5aafd1796c9e917e4f9d47
Bruno Oliveira <ylhito0307@gmail.com>
2025-09-18 17:41:11 +0000

    Error: ./src/components/report-form.tsx:200:6 Parsing ecmascript source

--- src/components/report-form.tsx
+++ src/components/report-form.tsx
@@ -264,4 +264,8 @@ export default function ReportForm() {
                   <Send className="mr-2 h-4 w-4" />
                 )}
                 Enviar Ocorrência
-              
+            </Button>
+        </div>
+    </form>
+  );
+}
